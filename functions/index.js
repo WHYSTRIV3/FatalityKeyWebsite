c771d5f56a166b1b2a2d59aa205d81d03842a780
FATALITY <117491501+WHYSTRIV3@users.noreply.github.com>
2024-07-12 17:05:32 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,14 @@
-const functions = require("firebase-functions");
-const express = require("express");
-const request = require("request");
-const cors = require("cors");
+const express = require('express');
+const request = require('request');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
 // Proxy endpoint
-app.get("/keys", (req, res) => {
-    const firebaseUrl = 'https://fatalitykey-default-rtdb.firebaseio.com/keys.json';
+app.get('/keys', (req, res) => {
+    const firebaseUrl = 'https://fatalitykey-default-rtdb.firebaseio.com/keys.json'; // Replace with your Firebase URL
     request(firebaseUrl, (error, response, body) => {
         if (!error && response.statusCode === 200) {
             res.send(body);
@@ -19,4 +18,6 @@ app.get("/keys", (req, res) => {
     });
 });
 
-exports.api = functions.https.onRequest(app);
+app.listen(PORT, () => {
+    console.log(`Proxy server running on port ${PORT}`);
+});
