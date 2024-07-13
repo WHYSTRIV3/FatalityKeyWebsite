15d41ba8c8a6e792384698296c33fa18d3835fd4
FATALITY <117491501+WHYSTRIV3@users.noreply.github.com>
2024-07-12 23:16:20 -0400

    Update generate-key.js

--- api/generate-key.js
+++ api/generate-key.js
@@ -4,6 +4,23 @@ const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 module.exports = async (req, res) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    // Handle OPTIONS request
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     if (req.method === 'POST') {
         try {
             await client.connect();
